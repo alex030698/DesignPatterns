f234c5ac0e772287e2d0a50a34544baae4683444
alex030698 <popescualexandru0306@gmail.com>
2024-04-17 17:26:03 +0300

    Initial Commit

---
+++ TestRepo/main.cpp
@@ -0,0 +1,14 @@
+//
+//  main.cpp
+//  TestRepo
+//
+//  Created by Popescu Alexandru on 17.04.2024.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
