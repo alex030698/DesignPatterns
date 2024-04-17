87a051c8557d5d09b37a47b6d99e6ec11e42c108
alex030698 <popescualexandru0306@gmail.com>
2024-04-17 17:29:54 +0300

    Update file structure and gitignore

---
+++ DesignPatterns/main.cpp
@@ -0,0 +1,14 @@
+//
+//  main.cpp
+//  TestRepo
+//
+//  Created by Popescu Alexandru on 17.04.2024.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Start design patterns training"<<std::endl;
+    return 0;
+}
---
+++ test.h
@@ -0,0 +1,12 @@
+//
+//  test.h
+//  Design Paterns
+//
+//  Created by Popescu Alexandru on 17.04.2024.
+//
+
+#ifndef test_h
+#define test_h
+
+
+#endif /* test_h */
